240e32f10dc35b29332dbe4e5d99a4dbaf83b2a2
aryannsainiii-hub <aryannsainiii@gmail.com>
2026-09-08 12:09:21 +0530

    routes

--- BACKEND/exp4.js
+++ BACKEND/exp4.js
@@ -1,9 +1,35 @@
 const http = require("http");
 
-const server = http.createServer((req, res) => {
-    res.write("hello");
-    res.end("hello");
-})
+const users = [
+  {
+    id: 1,
+    name: "Aryan",
+    role: "Student",
+  },
+  {
+    id: 2,
+    name: "Rahul",
+    role: "Developer",
+  },
+];
+
+const server = http.createServer((request, response) => {
+  // GET all users
+  if (request.url === "/users" && request.method === "GET") {
+    response.setHeader("Content-Type", "application/json");
+
+    response.end(JSON.stringify(users));
+  }
+
+  // POST a new user
+  else if (request.url === "/users" && request.method === "POST") {
+    response.end("User created successfully!");
+  } else {
+    response.statusCode = 404;
+    response.end("Route not found");
+  }
+});
+
 server.listen(3000, () => {
-    console.log("server is running on port 3000");
-})
+  console.log("Server running on http://localhost:3000");
+});
